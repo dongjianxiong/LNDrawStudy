2ab8164b326c4488b90233fd315718756f0a6bc3
scad-build <scad-build@sohu-inc.com>
2019-04-10 16:32:48 +0800

    Initial Commit

---
+++ LNDrawStudy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LNDrawStudy
+//
+//  Created by ioser on 2019/4/10.
+//  Copyright © 2019年 Lenny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LNDrawStudy/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LNDrawStudy
+//
+//  Created by ioser on 2019/4/10.
+//  Copyright © 2019年 Lenny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
